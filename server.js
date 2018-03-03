feb846d3cf40f5f51058ca0d55bdcacbbf15df90
Nils Mueller <nils@mueller.name>
2018-03-03 14:42:32 +0100

    Change: refactor server.js, added index.html and .editorconfig

--- server.js
+++ server.js
@@ -1,48 +1,60 @@
-let config = require('./config.json');
-let express = require('express');
-let app = express();
+import path from 'path'
+import express from 'express'
+import MQTTClient from 'async-mqtt'
 
-let data = {};
-let MQTTClient = require('async-mqtt');
-let mqtt =  MQTTClient.connect(config.mqtt.url);
+let config = require('./config.json')
+let counterTopics = [
+  '$SYS/broker/bytes/received',
+  '$SYS/broker/bytes/sent',
+  '$SYS/broker/messages/received',
+  '$SYS/broker/messages/sent',
+  '$SYS/broker/publish/messages/received',
+  '$SYS/broker/publish/messages/sent',
+  '$SYS/broker/publish/messages/dropped'
+]
+let data = {}
+let mqtt =  MQTTClient.connect(config.mqtt.url)
+
 mqtt.subscribe("$SYS/#");
+
 mqtt.on('message', (topic, message) => {
-    message = message.toString('utf8').replace(' seconds', '');
+    message = message.toString('utf8').replace(' seconds', '')
     if (!data[topic]) {
         data[topic] = {
             label: topic.replace('$SYS/', '').split('/').join('_').split(' ').join('_'),
-            type: message.indexOf('.') > 0 ? 'gauge' : 'counter'
+            type: counterTopics.includes(topic) ? 'counter' : 'gauge'
         }
     }
-    let intValue = parseInt(message);
-    data[topic].value = data[topic].type === 'counter' && intValue > 2147483647 ? parseInt(message).toExponential(8) : message;
-});
+    if (!message.includes('.') &&  message.length > 5) {
+        message = parseInt(message).toExponential(8)
+    }
+    data[topic].value = message
+})
 
-app.get('/', function (req, res) {
-    res.send('\n' +
-        '<html>\n' +
-        '<head><title>Mosquitto Exporter</title></head>\n' +
-        '<body>\n' +
-        '<h1>Mosquitto Exporter</h1>\n' +
-        '<p><a href="/metrics">Metrics</a></p>\n' +
-        '</body>\n' +
-        '</html>\n');
-});
+let app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 app.get('/metrics', function (req, res) {
-    let string = '';
-    for (let topic in data) {
-        if (['$SYS/broker/version', '$SYS/broker/timestamp'].indexOf(topic) >= 0) {
-            continue
-        }
-        string += `# HELP ${data[topic].label} ${topic}\n` +
-            `# TYPE ${data[topic].label} ${data[topic].type}\n` +
-            `${data[topic].label} ${data[topic].value}\n`
+  let connected = mqtt._client.connected;
+  let str = '# HELP broker_connected_state exporter is connected to the broker or not\n' +
+    '# TYPE broker_connected_state gauge\n' +
+    `broker_connected_state ${connected ? 1 : 0}\n`;
+  for (let topic in data) {
+    if (['$SYS/broker/version', '$SYS/broker/timestamp'].includes(topic)) {
+      continue
     }
-    res.set('Content-Type', 'text/plain');
-    res.send(string);
-});
+    let value = connected ? data[topic].value : 0;
+    str += `# HELP ${data[topic].label} ${topic}\n` +
+      `# TYPE ${data[topic].label} ${data[topic].type}\n` +
+      `${data[topic].label} ${value}\n`
+  }
+  res.set('Content-Type', 'text/plain')
+  res.send(str);
+})
 
-app.listen(9234, function () {
-    console.log('http://localhost:9234');
-});
+app.listen(9234, () => {
+  console.log(`http://localhost:9234`)
+})
